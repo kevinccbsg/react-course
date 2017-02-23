076603aa23be98024e27162fae1089f7c9835826
kevinccbsg <kevin2906@hotmail.com>
2017-02-23 23:43:58 +0100

    ref to get a reference of components

--- src/App.js
+++ src/App.js
@@ -5,17 +5,38 @@ const data = '!!!';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.handleClick = this.handleClick.bind(this);
+    this.state = { a: '', b: ''};
+    this.update = this.update.bind(this);
   }
-
+  update() {
+    this.setState({
+      a: this.a.refs.input.value,
+      b: this.b.value,
+    });
+  }
   handleClick() {
     alert('clicked');
   }
-
   render () {
     return (
-      <Button click={this.handleClick}>React</Button>
+      <div>
+        <Input 
+          ref={ component => this.a = component }
+          update={this.update}
+        /> {this.state.a}
+        <input 
+          ref={ node => this.b = node }
+          type="text"
+          onChange={this.update}
+        /> {this.state.b}
+      </div>
     );
+  }
+}
+
+class Input extends Component {
+  render() {
+    return <input ref="input" type="text" onChange={this.props.update} />;
   }
 }
 
